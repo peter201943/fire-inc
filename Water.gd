4a980b505d0c0d22eca29b6d8de04597dad6e6cf
Taha <tahagabre@gmail.com>
2020-06-07 13:07:43 -0400

    cleaned merge conflict

--- Water.gd
+++ Water.gd
@@ -2,6 +2,8 @@ extends KinematicBody
 
 export var distance = 100
 export var radius = 20
+export(Vector3) var gravity = Vector3(0, -9.8, 0)
+
 var dir
 
 func _ready():
@@ -10,7 +12,8 @@ func _ready():
 func _process(delta):
 	var colliding
 	
-	# put logic here for gravity drop!
+	# emulating gravity
+	#translate(gravity)
 	
 	if Input.is_action_pressed("ui_accept"):
 		var move = Vector3(distance, 0, dir)

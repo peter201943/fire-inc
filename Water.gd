13b0fc9d59bdb2808e98de28cc859b1ec44f70fa
Taha <tahagabre@gmail.com>
2020-06-07 11:56:39 -0400

    water spheres move more like a cone

--- Water.gd
+++ Water.gd
@@ -1,15 +1,15 @@
 extends KinematicBody
 
-export var distance = 5
+export var distance = 100
+var dir
 
 func _ready():
-	pass
+	dir = rand_range(-3, 3)
 
 func _process(delta):
 	# determine the vector within the cone the sphere will follow
-	var dir = rand_range(-.5, .5)
 	
 	# if input is pushed send the sphere down the vector path
 	if Input.is_action_pressed("ui_accept"):
 		var move = Vector3(distance, 0, dir)
-		var colliding = move_and_collide(move*delta)
+		var colliding = move_and_collide(move * delta)

75d284dd5e1026964e245c8ac0f5f90738d4bc91
Frank <fjm52@drexel.edu>
2020-06-07 13:45:07 -0400

    Started window logic in window script.

--- asset/script/window.gd
+++ asset/script/window.gd
@@ -1,12 +1,28 @@
 extends Spatial
 
 var on_fire = false
+var burn_rate = 1
+var health = 100
+var dead = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if not on_fire:
+		pass #set fire effects?
+	
+	if not dead and on_fire:
+		health -= burn_rate
+		#set fire effects?
+	if health <= 0:
+		dead = true
+		
+	if dead:
+		pass #TODO
+
+
+func set_onfire(b):
+	on_fire = b

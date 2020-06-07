fc51135c418ad687be9e5da0cd1541086ace1c0b
Taha <tahagabre@gmail.com>
2020-06-07 12:30:03 -0400

    water spheres will reset their position on collision

--- Water.gd
+++ Water.gd
@@ -1,15 +1,21 @@
 extends KinematicBody
 
 export var distance = 100
+export var radius = 20
 var dir
 
 func _ready():
-	dir = rand_range(-3, 3)
+	dir = rand_range(-radius, radius)
 
 func _process(delta):
-	# determine the vector within the cone the sphere will follow
+	var colliding
 	
-	# if input is pushed send the sphere down the vector path
+	# put logic here for gravity drop!
+	
 	if Input.is_action_pressed("ui_accept"):
 		var move = Vector3(distance, 0, dir)
-		var colliding = move_and_collide(move * delta)
+		colliding = move_and_collide(move * delta)
+	
+	if colliding:
+		set_global_transform(Transform.IDENTITY)
+		dir = rand_range(-radius, radius)

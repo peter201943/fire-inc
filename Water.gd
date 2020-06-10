05d3602118693056cdcbd27918054b4e7e9f8bba
Taha <tahagabre@gmail.com>
2020-06-09 20:15:43 -0400

    shoot water

--- Water.gd
+++ Water.gd
@@ -1,24 +1,10 @@
-extends KinematicBody
+extends RigidBody
 
 export var distance = 100
 export var radius = 20
-export(Vector3) var gravity = Vector3(0, -9.8, 0)
 
-var dir
+func reset(i):
+	set_global_transform(Transform.IDENTITY)
 
 func _ready():
-	dir = rand_range(-radius, radius)
-
-func _process(delta):
-	var colliding
-	
-	# emulating gravity
-	#translate(gravity)
-	
-	if Input.is_action_pressed("ui_accept"):
-		var move = Vector3(distance, 0, dir)
-		colliding = move_and_collide(move * delta)
-	
-	if colliding:
-		set_global_transform(Transform.IDENTITY)
-		dir = rand_range(-radius, radius)
+	connect("body_entered", self, "reset")
